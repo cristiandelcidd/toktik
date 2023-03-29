605c6e78db117a17efd0f092ff62ba515cb85b3f
Cristian Delcid <cristian_delcid10@yahoo.com>
2023-03-29 11:24:49 -0600

    Repositories & Datasources (agregado)

---
+++ lib/infrastucture/repositories/video_post_repository_impl.dart
@@ -0,0 +1,19 @@
+import 'package:toktik/domain/datasources/video_post_datasource.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/domain/repositories/video_post_repository.dart';
+
+class VideoPostsRepositoryImplementation implements VideoPostRepository {
+  final VideoPostDatasource videosDatasource;
+
+  VideoPostsRepositoryImplementation({required this.videosDatasource});
+
+  @override
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userId) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideosByPage(int page) {
+    throw videosDatasource.getTrendingVideosByPage(page);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:toktik/config/theme/app_theme.dart';
+import 'package:toktik/infrastucture/datasources/local_video_datasource_impl.dart';
+import 'package:toktik/infrastucture/repositories/video_post_repository_impl.dart';
 import 'package:toktik/presentation/providers/discover_provider.dart';
 import 'package:toktik/presentation/screens/discover/discover_screen.dart';
 
@@ -12,11 +14,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final videoPostRepository = VideoPostsRepositoryImplementation(
+        videosDatasource: LocalVideoDatasource());
+
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
           lazy: false,
-          create: (_) => DiscoverProvider()..loadNextPage(),
+          create: (_) => DiscoverProvider(videosRepository: videoPostRepository)
+            ..loadNextPage(),
         )
       ],
       child: MaterialApp(
